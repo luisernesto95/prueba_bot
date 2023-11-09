435e4e1c93e0da60a935c8d87d7768bf6fb15541
luisernesto95 <148005068+luisernesto95@users.noreply.github.com>
2023-11-09 14:43:11 +0100

    confi.py

--- confi.py
+++ confi.py
@@ -1,3 +1,3 @@
-Bot_token = "5827190779:AAG_e6U2HkzVLZ38yNuqm7RRPkgUp_PDMGw"
-chanel_id = -1001858527257
+Bot_token = "6597344460:AAFk37dxcD6ekFq4owpR2jpL-RvjIIRf2CE"
+chanel_id = -1001807371414
 db_access = 9 #value int de la db, desmarcar line 1531 bot.py
